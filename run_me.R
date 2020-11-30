cc12283b7466b25fcb48168e7a9770674aedbce8
carllutnaes <carl.lutnaes@gmail.com>
2020-11-30 10:58:17 +0100

    Updated project for simplification

--- run_me.R
+++ run_me.R
@@ -6,15 +6,11 @@
 # Descriptive scripts to make LaTeX plots not included
 # because they 
 
-setwd("~/Documents/Programming/GitHub/BSc Thesis/")
+setwd("~/Documents/GitHub/replicating_anomaly_ROE/")
 data_dir <- "./formatted_data/"
 
 source("load_data_nsdq.R")
 
-source("calculate_returns_all_ew.R")
-source("calculate_returns_all_vw.R")
-
-source("calculate_returns_nasdaq_ew.R")
 source("calculate_returns_nasdaq_vw.R")
 
 source("desc_stat_breakpoint.R")
